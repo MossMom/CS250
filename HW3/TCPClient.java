0bcd4675c7eeb4b68c8782de65c9b77c2d84ba30
Mossy Jimmerson <mossmom@colostate.edu>
2023-11-05 20:45:03 -0700

    HW 1 Submission 1

--- HW3/TCPClient.java
+++ HW3/TCPClient.java
@@ -2,35 +2,67 @@ package HW3;
 
 import java.net.*;
 import java.io.*;
+import java.util.Random;
 
 /*
  * Exit Codes:
  * ~1~ Incorrect number of args provided to main()
+ * ~2~ Error while connected to server
  */
 
 public class TCPClient {
     private static Socket socket;
     private static DataInputStream din = null;
-    public static void main(String[] args){
-        String serverHost = null;
-        int serverPort = Integer.MAX_VALUE;
-        if (args.length == 2){
-            serverHost = args[0];
-            serverPort = Integer.parseInt(args[1]);
-        } else {
-            System.err.println("Incorrect Number of Arguments. 2 Arguments Required. java Client <serverHost> <serverPort>");
-            System.exit(1);
-        }
+    private static String serverHost = null;
+    private static int serverPort = Integer.MAX_VALUE;
+    private static int numMessages;
+    private static int seed;
+    private static Random rng = new Random();
+
+// TASK 1
+    public static void serverSetup(String serverHost, int serverPort) {
         try {
             socket = new Socket(serverHost, serverPort);
-            System.out.println("Connected to Server!");
+            //System.out.println("Connected to Server!");
             din = new DataInputStream(socket.getInputStream());
-            int message = din.readInt();
-            System.out.println("Message from server: " + message);
-            System.out.println("Terminating.");
         } catch (IOException e) {
             System.err.println("Fatal Connection Error!");
             e.printStackTrace();
         }
+    }
+
+// TASK 2
+    public static void recieveConfig(String serverHost, int serverPort) {
+        try {
+            numMessages = din.readInt();
+            //System.out.println("numMessages \"" + numMessages + "\" recieved from server!");
+
+            seed = din.readInt();
+            //System.out.println("seed \"" + seed + "\" recieved from server!");
+            rng.setSeed(seed);
+
+            System.out.println("Recieved config");
+            System.out.println("number of messages = " + numMessages);
+            System.out.println("seed = " + seed);
+        } catch (IOException e) {
+            System.err.println("Fatal Connection Error!");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args){
+        //String[] args2 = {"bee.cs.colostate.edu", "8008"};
+
+        if (args.length == 2){
+            serverHost = args[0];
+            serverPort = Integer.parseInt(args[1]);
+        } else {
+            System.err.println("Incorrect Number of Arguments. 2 Arguments Required.");
+            System.exit(1);
+        }
+
+        serverSetup(serverHost, serverPort);
+
+        recieveConfig(serverHost, serverPort);
     }
 }
--- HW3/TCPServer.java
+++ HW3/TCPServer.java
@@ -3,11 +3,13 @@ package HW3;
 import java.net.*;
 import java.io.*;
 import java.util.Random;
+import java.util.HashMap;
 
 /*
  * Exit Codes:
  * ~1~ Incorrect number of args provided to main()
- * ~2~ Exception thrown in serverSetup
+ * ~2~ Exception thrown in serverSetup()
+ * ~3~ Exception thrown in sendReqToClient()
  */
 
 public class TCPServer {
@@ -15,52 +17,94 @@ public class TCPServer {
     private static ServerSocket serverSocket;
     private static DataOutputStream doutClient1 = null;
     private static DataOutputStream doutClient2 = null;
+    private static HashMap<String, DataOutputStream> outputStreams = new HashMap<String, DataOutputStream>();
+    private static Random rng = new Random();
+    private static String clientOneName;
+    private static String clientTwoName;
 
 // TASK 1
     public static void serverSetup(int portNumber, int seed, Random rng) {
         try {
+            System.out.println("IP Address: " + InetAddress.getLocalHost());
+            System.out.println("Port Number " + portNumber);
+
             serverSocket = new ServerSocket(portNumber);
-            //wait on client
+
+            System.out.println("waiting for client...");
+            
+            //System.out.println("Waiting on Client 1");
             clientSocket = serverSocket.accept();
-            System.out.println("Connected to Client(s)!"); //TODO Remove
+            //System.out.println("Connected to Client 1!");
+            doutClient1 = new DataOutputStream(clientSocket.getOutputStream());
+            outputStreams.put("C1", doutClient1);
+            clientOneName = clientSocket.getInetAddress().getHostName();
+            //System.out.println("Client 1 added to outputStreams HashMap!");
+
+            //System.out.println("Waiting on Client 2");
+            clientSocket = serverSocket.accept();
+            //System.out.println("Connected to Client 2!");
+            doutClient2 = new DataOutputStream(clientSocket.getOutputStream());
+            outputStreams.put("C2", doutClient2);
+            clientTwoName = clientSocket.getInetAddress().getHostName();
+            //System.out.println("Client 2 added to outputStreams HashMap!");
+
+            System.out.println("Clients Connected!");
+
             rng.setSeed(seed);
-            System.out.println("Random seed set to <seed>!"); //TODO Remove
+            //System.out.println("Random seed set to " + seed + "!");
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(2);
         }
     }
 // TASK 2
-    public static void sendReqToClient(int numMessages, Socket clientSocket) {
+    public static void sendConfig(int numMessages, Socket clientSocket, int portNumber, Random rng) {
         try {
-            doutClient1 = new DataOutputStream(clientSocket.getOutputStream());
-            doutClient1.writeInt(numMessages);
-            doutClient1.flush();
-            System.out.println("Information Sent to Client 1!");
+            System.out.println("Sending config to clients...");
+
+            outputStreams.get("C1").writeInt(numMessages);
+            outputStreams.get("C1").flush();
+            int clientOneSeed = rng.nextInt();
+            outputStreams.get("C1").writeInt(clientOneSeed);
+            outputStreams.get("C1").flush();
+            //System.out.println(numMessages + " & random int sent to Client 1!");
+
+            System.out.println(clientOneName + " " + clientOneSeed);
         } catch (IOException e) {
-            // TODO: handle exception
+            System.err.println("Client 1 could not listen on port: " + portNumber);
+            System.exit(3);
         }
         try {
-            doutClient2 = new DataOutputStream(clientSocket.getOutputStream());
-            doutClient2.writeInt(numMessages);
-            doutClient2.flush();
-            System.out.println("Information Sent to Client 2!");
+            outputStreams.get("C2").writeInt(numMessages);
+            outputStreams.get("C2").flush();
+            int clientTwoSeed = rng.nextInt();
+            outputStreams.get("C2").writeInt(clientTwoSeed);
+            outputStreams.get("C2").flush();
+            //System.out.println(numMessages + " & random int sent to Client 2!");
+
+            System.out.println(clientTwoName + " " + clientTwoSeed);
         } catch (IOException e) {
-            // TODO: handle exception
+            System.err.println("Client 2 could not listen on port: " + portNumber);
+            System.exit(3);
         }
+        
+        System.out.println("Finished sending config to clients.");
+
     }
 
     public static void main(String[] args){
+        //String[] args2 = {"8008", "69", "3"};
+
         int portNumber = Integer.MAX_VALUE; // Garbage value so we don't start server on random port.
         int seed = 0;
         int numMessages = 0;
-        Random rng = new Random(); // Init random class
         if (args.length == 3){
             portNumber = Integer.parseInt(args[0]);
             seed = Integer.parseInt(args[1]);
             numMessages = Integer.parseInt(args[2]);
         } else {
-            System.err.println("Incorrect Number of Arguments. 3 Arguments Required. java Server <portNumber>");
+            System.err.println("Incorrect Number of Arguments. 3 Arguments Required.");
             System.exit(1);
         }
 
@@ -68,22 +112,7 @@ public class TCPServer {
         serverSetup(portNumber, seed, rng);
 
         // TASK 2
-        //sendReqToClient(clientSocket1, clientSocket2);
+        sendConfig(numMessages, clientSocket, portNumber, rng);
 
-        try {
-            serverSocket = new ServerSocket(portNumber);
-            //wait on client
-            clientSocket = serverSocket.accept();
-            System.out.println("Connected to Client!");
-            int theAnswer = 42;
-            doutClient1 = new DataOutputStream(clientSocket.getOutputStream());
-            doutClient1.writeInt(theAnswer);
-            doutClient1.flush();
-            System.out.println("Information Sent to Client.");
-            System.out.println("Terminating.");
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: " + portNumber);
-            System.exit(1);
-        }
     }
 }
